577522cdb1157e1aca9af6100f8960bb28d77e64
Vijendran Iyer <vijendraniyer@gmail.com>
2024-09-12 10:19:44 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,102 +1,119 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoundexTestsuite, BasicName) {
+// Test case for generating Soundex code from a basic name
+TEST(SoundexTestsuite, GeneratesSoundexForBasicName) {
     char soundex[5];
     generateSoundex("Vijendran", soundex);
     ASSERT_STREQ(soundex, "V253");
 }
 
-TEST(SoundexTestsuite, MixedCaseName) {
+// Test case for generating Soundex code from a name with mixed case
+TEST(SoundexTestsuite, HandlesMixedCaseName) {
     char soundex[5];
     generateSoundex("vIjeNDran", soundex);
     ASSERT_STREQ(soundex, "V253");
 }
 
-TEST(SoundexTestsuite, EmptyString) {
+// Test case for generating Soundex code from an empty string
+TEST(SoundexTestsuite, HandlesEmptyString) {
     char soundex[5];
     generateSoundex("", soundex);
     ASSERT_STREQ(soundex, "0000");
 }
 
-TEST(SoundexTestsuite, SingleCharacter) {
+// Test case for generating Soundex code from a single character
+TEST(SoundexTestsuite, HandlesSingleCharacterName) {
     char soundex[5];
     generateSoundex("A", soundex);
     ASSERT_STREQ(soundex, "A000");
 }
 
-TEST(SoundexTestsuite, SingleNonAlphabeticCharacter) {
+// Test case for generating Soundex code from a single non-alphabetic character
+TEST(SoundexTestsuite, HandlesSingleNonAlphabeticCharacter) {
     char soundex[5];
     generateSoundex("1", soundex);
     ASSERT_STREQ(soundex, "1000");
 }
 
-TEST(SoundexTestsuite, SingleLowercaseCharacter) {
+// Test case for generating Soundex code from a single lowercase character
+TEST(SoundexTestsuite, HandlesSingleLowercaseCharacter) {
     char soundex[5];
     generateSoundex("a", soundex);
     ASSERT_STREQ(soundex, "A000");
 }
 
-TEST(SoundexTestsuite, RepeatedConsonants) {
+// Test case for generating Soundex code from a name with repeated consonants
+TEST(SoundexTestsuite, HandlesRepeatedConsonants) {
     char soundex[5];
     generateSoundex("AABB", soundex);
     ASSERT_STREQ(soundex, "A100");
 }
 
-TEST(SoundexTestsuite, ConsecutiveIdenticalConsonants) {
+// Test case for generating Soundex code from a name with consecutive identical consonants
+TEST(SoundexTestsuite, HandlesConsecutiveIdenticalConsonants) {
     char soundex[5];
     generateSoundex("AABBB", soundex);
     ASSERT_STREQ(soundex, "A100");
 }
 
-TEST(SoundexTestsuite, NonAlphabeticCharacters) {
+// Test case for generating Soundex code from a name with non-alphabetic characters
+TEST(SoundexTestsuite, HandlesNonAlphabeticCharacters) {
     char soundex[5];
     generateSoundex("A1B2C", soundex);
     ASSERT_STREQ(soundex, "A120");
 }
 
-TEST(SoundexTestsuite, SpecialCharacters) {
+// Test case for generating Soundex code from a name with special characters
+TEST(SoundexTestsuite, HandlesSpecialCharacters) {
     char soundex[5];
     generateSoundex("A-B_C", soundex);
     ASSERT_STREQ(soundex, "A120");
 }
 
-TEST(SoundexTestsuite, NameWithVowels) {
+// Test case for generating Soundex code from a name with only vowels
+TEST(SoundexTestsuite, HandlesNameWithOnlyVowels) {
     char soundex[5];
     generateSoundex("AeIou", soundex);
     ASSERT_STREQ(soundex, "A000");
 }
 
-TEST(SoundexTestsuite, ShortName) {
+// Test case for generating Soundex code from a short name
+TEST(SoundexTestsuite, HandlesShortName) {
     char soundex[5];
     generateSoundex("Jo", soundex);
     ASSERT_STREQ(soundex, "J000");
 }
 
-TEST(SoundexTestsuite, LongName) {
+// Test case for generating Soundex code from a long name
+TEST(SoundexTestsuite, HandlesLongName) {
     char soundex[5];
     generateSoundex("Alexander", soundex);
     ASSERT_STREQ(soundex, "A452");
 }
 
-TEST(SoundexTestsuite, LongSequenceOfSameConsonants) {
+// Test case for generating Soundex code from a long sequence of the same consonants
+TEST(SoundexTestsuite, HandlesLongSequenceOfSameConsonants) {
     char soundex[5];
     generateSoundex("BBBBBBBB", soundex);
     ASSERT_STREQ(soundex, "B111");
 }
 
-TEST(SoundexTestsuite, UpperBoundOfLength) {
+// Test case for generating Soundex code from a name at the upper bound of length
+TEST(SoundexTestsuite, HandlesUpperBoundOfNameLength) {
     char soundex[5];
     generateSoundex("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", soundex);
     ASSERT_STREQ(soundex, "A000");
 }
 
-TEST(SoundexTestsuite, NullInputSoundexArray) {
+// Test case for handling NULL input for Soundex array
+TEST(SoundexTestsuite, HandlesNullInputForSoundexArray) {
     generateSoundex("Alexander", NULL);
     // No direct assertion here; ensures no crash occurs
 }
 
-TEST(SoundexTestsuite, NullInputName) {
+// Test case for handling NULL input for name
+TEST(SoundexTestsuite, HandlesNullInputForName) {
     char soundex[5];
     generateSoundex(NULL, soundex);
     ASSERT_STREQ(soundex, "0000");
